6b1fb340d64161f296b3034336166de63f32e580
beemo <lsj4401@coupang.com>
2018-02-12 23:05:18 +0900

    init spring application

---
+++ src/main/java/com/sa/blockchain/sablockchain/SaBlockchainApplication.java
@@ -0,0 +1,12 @@
+package com.sa.blockchain.sablockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaBlockchainApplication.class, args);
+	}
+}
